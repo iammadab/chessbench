10b6da37a19dc1dae81dd7cad7eb6c465bd0ff99
Wisdom Ogwu <iammadab@gmail.com>
2026-02-07 14:56:17 +0100

    make the picture local

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,3 +1,15 @@
+import bB from './assets/pieces/bB.svg'
+import bK from './assets/pieces/bK.svg'
+import bN from './assets/pieces/bN.svg'
+import bP from './assets/pieces/bP.svg'
+import bQ from './assets/pieces/bQ.svg'
+import bR from './assets/pieces/bR.svg'
+import wB from './assets/pieces/wB.svg'
+import wK from './assets/pieces/wK.svg'
+import wN from './assets/pieces/wN.svg'
+import wP from './assets/pieces/wP.svg'
+import wQ from './assets/pieces/wQ.svg'
+import wR from './assets/pieces/wR.svg'
 import './App.css'
 
 const sampleFen =
@@ -57,18 +69,18 @@ function parsePgnMoves(pgn: string) {
 }
 
 const pieceSrc: Record<string, string> = {
-  p: 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg',
-  r: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg',
-  n: 'https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg',
-  b: 'https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg',
-  q: 'https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg',
-  k: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg',
-  P: 'https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg',
-  R: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg',
-  N: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg',
-  B: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg',
-  Q: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg',
-  K: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg',
+  p: bP,
+  r: bR,
+  n: bN,
+  b: bB,
+  q: bQ,
+  k: bK,
+  P: wP,
+  R: wR,
+  N: wN,
+  B: wB,
+  Q: wQ,
+  K: wK,
 }
 
 function Piece({ code }: { code: string }) {
